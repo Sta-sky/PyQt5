07e231644c844b2e046f2149195032f16c303d3e
Sta-sky <56905658+Sta-sky@users.noreply.github.com>
2021-04-16 17:58:18 +0800

    Update set_table_color.py

--- set_table_color.py
+++ set_table_color.py
@@ -4,34 +4,40 @@ from PyQt5 import QtCore
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon, QBrush, QColor
 from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication, QTableWidget, QTableWidgetItem, QPushButton, \
-    QVBoxLayout
+    QVBoxLayout, QHBoxLayout, QFrame, QSplitter
 import sys
 
 
 class QlabelDemo(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.action = QAction(self)
-        self.setGeometry(QtCore.QRect(100, 200, 500, 600))
-        self.action.setStatusTip("我来了")
-        exitAction1 = QAction(QIcon('exit.png'), '&Exit', self)
-        exitAction1.setShortcut('Ctrl+Q')
+        self.resize(1250, 750)
+        self.setWindowTitle('SEMview')
+        self.addStretch(1)
+        self.menu()
 
-        exitAction2 = QAction(QIcon('exit.png'), '&Edit', self)
-        exitAction2.setShortcut('Ctrl+l')
+        self.mainlayout = QHBoxLayout(self)
+        self.right_top = QFrame()
+        self.right_top.setFrameShape(QFrame.StyledPanel)
+        self.right_botm = QFrame()
+        self.right_botm.setFrameShape(QFrame.Box)
+        self.left_box = QFrame()
+        self.left_box.setFrameShape(QFrame.StyledPanel)
+        self.left_box.setLineWidth(100)
+        self.splitter1 = QSplitter(Qt.Horizontal)
+        self.splitter1.addWidget(self.right_top)
+        self.splitter2 = QSplitter(Qt.Vertical)
+        self.splitter2.addWidget(self.left_box)
+        self.splitter2.addWidget(self.right_botm)
+        self.splitter3 = QSplitter(Qt.Horizontal)
+        self.splitter3.addWidget(self.splitter1)
+        self.splitter3.addWidget(self.splitter2)
 
-        exitAction1.setStatusTip('Exit application')
-        exitAction2.setStatusTip('Edit application')
-        exitAction1.triggered.connect(qApp.quit)
-        self.statusBar()
-        self.menubar = self.menuBar()
-        filemenu = self.menubar.addMenu('&file')
-        filemenu.addAction(exitAction1)
-        filemenu.addAction(exitAction2)
 
-        self.lable = QTableWidget(self)
+        self.lable = QTableWidget(self.right_botm)
         self.lable.setRowCount(4)
         self.lable.setColumnCount(3)
+
         self.lable.setGeometry(QtCore.QRect(10, 20, 300, 200))
         for i in range(4):
             for j in range(3):
@@ -46,6 +52,24 @@ class QlabelDemo(QMainWindow):
         # 设置默认为降序排列
         self.button.clicked.connect(self.order)
         self.orderType = Qt.DescendingOrder
+
+
+
+    def menu(self):
+        self.action = QAction(self)
+        self.action.setStatusTip("我来了")
+        exitAction1 = QAction(QIcon('exit.png'), '&Exit', self)
+        exitAction1.setShortcut('Ctrl+Q')
+        exitAction2 = QAction(QIcon('exit.png'), '&Edit', self)
+        exitAction2.setShortcut('Ctrl+l')
+        exitAction1.setStatusTip('Exit application')
+        exitAction2.setStatusTip('Edit application')
+        exitAction1.triggered.connect(qApp.quit)
+        self.statusBar()
+        self.menubar = self.menuBar()
+        filemenu = self.menubar.addMenu('&file')
+        filemenu.addAction(exitAction1)
+        filemenu.addAction(exitAction2)
 
     def order(self):
         if self.orderType == Qt.DescendingOrder:
