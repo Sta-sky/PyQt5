d49bf85724d0be91499bcb3bd1abc3432fe7b1e3
kb_dyy <holddang@sina.com>
2025-04-28 16:16:43 +0800

    update

--- 控件总结/快捷键通信.py
+++ 控件总结/快捷键通信.py
@@ -4,12 +4,17 @@ from PyQt5.QtGui import QKeySequence
 from PyQt5.QtWidgets import QWidget, QShortcut, QLabel, QApplication, QHBoxLayout
 
 
+import sys
+from PyQt5.QtWidgets import QWidget, QShortcut, QLabel, QApplication, QHBoxLayout
+from functools import partial
+
+
 class Window(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         # 初始化界面
-        self.label = QLabel("Try Ctrl+O, Shift+O", self)
+        self.label = QLabel("Try Ctrl+F, Shift+O", self)
         self.layout = QHBoxLayout(self)
         self.layout.addWidget(self.label)
 
@@ -22,27 +27,18 @@ class Window(QWidget):
     def _setup_shortcuts(self):
         # Ctrl+F 快捷键
         self.shortcut_ctrl = QShortcut(QKeySequence("Ctrl+F"), self)
-        self.shortcut_ctrl.activated.connect(self.on_open)
+        self.shortcut_ctrl.activated.connect(partial(self.on_open, "Ctrl+F"))
 
         # Shift+O 快捷键
         self.shortcut_shift = QShortcut(QKeySequence("Shift+O"), self)
-        self.shortcut_shift.activated.connect(self.on_open_shift)
+        self.shortcut_shift.activated.connect(partial(self.on_open, "Shift+O"))
 
         # Alt+O 快捷键
         self.shortcut_alt = QShortcut(QKeySequence("Alt+O"), self)
-        self.shortcut_alt.activated.connect(self.on_open_alt)
+        self.shortcut_alt.activated.connect(partial(self.on_open, "Alt+O"))
 
-    @pyqtSlot()
-    def on_open(self):
-        print("Ctrl+F Triggered!")
-
-    @pyqtSlot()
-    def on_open_shift(self):
-        print("Shift+O Triggered!")
-
-    @pyqtSlot()
-    def on_open_alt(self):
-        print("Alt+O Triggered!")
+    def on_open(self, key):
+        print(f"{key} Triggered!")
 
 
 if __name__ == "__main__":
